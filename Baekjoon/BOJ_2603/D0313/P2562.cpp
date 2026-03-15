99e0acc06d49c985be8b8f4080e20f6de75496bc
이지민 <leechimins@naver.com>
2026-03-15 19:18:55 +0900

    📦백준 폴더 생성

---
+++ Baekjoon/BOJ_2603/D0313/P2562.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	vector<int> v;
+	v.push_back(1);
+	v.push_back(7);
+	v.push_back(3);
+	v.push_back(5);
+	v.push_back(9);
+
+	sort(v.begin(), v.end(), greater<int>());
+	for (int x : v)
+		cout << x << " ";
+	cout << "\n";
+
+	auto it = lower_bound(v.begin(), v.end(), 4);
+
+	cout << it - v.begin();
+
+	return 0;
+}
