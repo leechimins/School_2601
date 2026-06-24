b08472b11014b4ea6d07b40f8aa64f98d830d354
이지민 <leechimins@naver.com>
2026-06-24 13:36:47 +0900

    📑VR/AR1: 기말 과제5_화살3_문제 상황

--- VRAR/Final_Project/Assets/Scripts/ArrowController.cs
+++ VRAR/Final_Project/Assets/Scripts/ArrowController.cs
@@ -2,20 +2,39 @@ using UnityEngine;
 
 public class ArrowController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float speed;
+    Vector3 moveDirection;
+
     void Start()
     {
-        
+        speed = 8.0f;
+        Destroy(gameObject, 3f);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(moveDirection * speed * Time.deltaTime);
+
+        Vector3 viewPos = Camera.main.WorldToViewportPoint(transform.position);
+        if (viewPos.x < 0f || viewPos.x > 1f || viewPos.y < 0f || viewPos.y > 1f)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Orc"))
+        {
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
     }
 
     public void Init(Vector2 dir)
     {
-
+        moveDirection = new Vector3(dir.x, dir.y, 0).normalized;
+        float angle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
     }
 }
